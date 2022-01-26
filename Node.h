6250cec22d2ccb57fecbb7065573f2a2d9ebb612
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2022-01-26 06:14:01 -0600

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,8 @@
+#ifndef NODE_H
+#define NODE_H
+#include "NetworkPacket.h"
+
+// TODO: Define the Node class
+// Note: Since the functions are all small, you can put everything in this file
+
+#endif
---
+++ Queue.h
@@ -0,0 +1,2 @@
+// TODO: Define the Queue class
+// Note: You can do everything in this file since the functions can all be very small (hint: use your list funcitons!)
