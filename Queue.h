ff7e8aab0231c0261695fb8f5ffc95d731e11c90
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2022-03-05 10:03:43 -0600

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,2 +1,29 @@
-// TODO: Define the Queue class
-// Note: You can do everything in this file since the functions can all be very small (hint: use your list funcitons!)
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "DoublyLinkedList.h"
+#include <iostream>
+using namespace std;
+// Implementing queue which is used in the simulation of first come first serve network router
+class Queue {
+    private:
+        DoublyLinkedList dll; // Queue will be implemented using the doubly linked list.
+    public:
+        bool empty() {
+            if (dll.getLength() == 0) 
+            {   // If length is 0, queue is empty.
+                return true;
+            }
+            return false; // Otherwise the queue has data.
+        }
+        void push_back(NetworkPacket data) {
+            // Add the network packet at the tail of the linked list.
+            dll.insert(data, dll.getLength()); // Length value will add at tail of list.
+        }
+        NetworkPacket pop_front() { // Return the front element of the linked list.
+            return dll.remove(0);
+        }
+        string toString() { // Returns the contents of the queue as a string.
+            return dll.toString();
+        }
+};
+#endif
