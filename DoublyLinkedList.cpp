39dcf96437f91209006258183fdfd8f3dec84a51
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2022-04-19 08:26:21 -0500

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include "NetworkPacket.h"
 #include "DoublyLinkedList.h"
 #include "Node.h"
+
 using namespace std;
+// Implement the templates.
 // Implement the copy constructor.
-DoublyLinkedList::DoublyLinkedList(const DoublyLinkedList& other)
+template <typename T>
+DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList<T>& other)
     : length(0), front(nullptr), back(nullptr)
 {
     // Check if the list is empty.
@@ -15,11 +19,11 @@ DoublyLinkedList::DoublyLinkedList(const DoublyLinkedList& other)
         return;
     }
     // Create temp pointer for traversing the source list.
-    Node* curr = other.front;
+    Node<T>* curr = other.front;
     int index_count = 0; // Counter for keeping track of where to insert.
     while (curr != nullptr)
     {
-        NetworkPacket pack = curr->getData(); // Get current nodes data.
+        T pack = curr->getData(); // Get current nodes data.
         this->insert(pack, index_count); // Perform the insertion.
         index_count++; // Increment counter and curr.
         curr = curr->getNext();
@@ -27,18 +31,19 @@ DoublyLinkedList::DoublyLinkedList(const DoublyLinkedList& other)
 }
 
 // Implement the copy assignment constructor.
-DoublyLinkedList& DoublyLinkedList::operator=(const DoublyLinkedList& other) {
+template <typename T>
+DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const DoublyLinkedList& other) {
     // First do check for self assignment.
     if (this != &other)
     {
         // Clear current objects memory to avoid memory leak.
         this->clear();
         // Create temp pointer for traversing the source list.
-        Node* curr = other.front;
+        Node<T>* curr = other.front;
         int index_count = 0; // Counter for keeping track of where to insert.
         while (curr != nullptr)
         {
-            NetworkPacket pack = curr->getData(); // Get current nodes data.
+            T pack = curr->getData(); // Get current nodes data.
             this->insert(pack, index_count); // Perform the insertion.
             index_count++; // Increment counter and curr.
             curr = curr->getNext();
@@ -48,19 +53,21 @@ DoublyLinkedList& DoublyLinkedList::operator=(const DoublyLinkedList& other) {
 }
 
 // Implement the destructor. (use clear helper method)
-DoublyLinkedList::~DoublyLinkedList() {
+template <typename T>
+DoublyLinkedList<T>::~DoublyLinkedList() {
     this->clear();
 }
 
 //  Defining the insert function to add a node to the list.
-void DoublyLinkedList::insert(NetworkPacket packet, int index) {
+template <typename T>
+void DoublyLinkedList<T>::insert(T packet, int index) {
     // Verify that index is in range, if not throw exception.
     if (index < 0 || index > length) 
     {
         throw std::out_of_range("Index outside of list bounds");
     }
     // Create new node for insertion.
-    Node* new_node = new Node(packet);
+    Node<T>* new_node = new Node<T>(packet);
     // Check if head is null (empty list).
     if (front == nullptr)
     {
@@ -76,13 +83,13 @@ void DoublyLinkedList::insert(NetworkPacket packet, int index) {
         back->setNext(new_node);
         new_node->setPrev(back);
         back = new_node;
-    } else { // Inserting in to the middle or end of list.
+    } else { // Inserting into the middle or end of list.
         // Traverse list to insertion point (create temp pointers).
-        Node* curr = front;
-        Node* prev = nullptr;
+        Node<T>* curr = front;
+        Node<T>* prev = nullptr;
         int count = 0; 
         while (curr != nullptr && count < index)
-        { // traverse through the list until at correct index.
+        { // Traverse through the list until at correct index.
             prev = curr;
             curr = curr->getNext();
             count++;
@@ -92,28 +99,22 @@ void DoublyLinkedList::insert(NetworkPacket packet, int index) {
         prev->setNext(new_node);
         new_node->setPrev(prev);
         curr->setPrev(new_node); 
-        
     }
     // Update size of list.
     length++;
 }
 
 // - toString() converts linked list into a string.
-string DoublyLinkedList::toString() {
-    // Create string stream variable and variables for packet components.
-    stringstream ss;
-    string source = "SRC: ";
-    string destination = "DST: ";
-    string checkSum = "CKS: ";
-    string data = "Data: ";
+// ** NOTE: this function would only currently work if 
+// T is type NetworkPacket
+template <typename T>
+string DoublyLinkedList<T>::toString() {
     // Create pointer for list traversal.
-    Node* curr = front;
+    std::stringstream ss;
+    Node<T>* curr = front;
     while (curr != nullptr) 
-    {   // Get the data from the Network packet inside of the node an add to ss.
-        ss << source << curr->getData().source << ", ";
-        ss << destination << curr->getData().destination << ", ";
-        ss << checkSum << curr->getData().checksum << ", ";
-        ss << data << curr->getData().data << "\n ";
+    {   
+        ss << curr->getData() << " ";
         curr = curr->getNext();
     }
     // Convert ss into string variable to return.
@@ -122,8 +123,9 @@ string DoublyLinkedList::toString() {
 }
 
 // Defining the remove() function which deletes a node from
-// the list at specified index. Must return the Network packet.
-NetworkPacket DoublyLinkedList::remove(int index) {
+// the list at specified index. Must return the packet.
+template <typename T>
+T DoublyLinkedList<T>::remove(int index) {
     // Throw exception if the list is empty.
     if (length == 0)
     {
@@ -140,8 +142,8 @@ NetworkPacket DoublyLinkedList::remove(int index) {
     // Need to remember to decrease the size before returning packet.
     if (index == 0)
     { // If index is 0 need to delete head and set the heads next as new head.
-        Node* curr = front;
-        NetworkPacket pack = curr->getData();
+        Node<T>* curr = front;
+        T pack = curr->getData();
         // Check if length is 1, then need to delete single element and
         // assign head and tail to null.
         if (length == 1) 
@@ -159,16 +161,16 @@ NetworkPacket DoublyLinkedList::remove(int index) {
         return pack;
     } else if (index == (length - 1)) // If index is (length - 1) we will have a new tail
     {
-        Node* curr = back;
+        Node<T>* curr = back;
         back->prev->next = nullptr;
         back = back->prev;
-        NetworkPacket pack = curr->getData();
+        T pack = curr->getData();
         delete curr;
         length--;
         return pack;
     } else { // If not deleting head or tail we are deleting in the middle somewhere. 
         // Set temp pointer for traversal.
-        Node* curr = front;
+        Node<T>* curr = front;
         // Set counter to track until we are at correct index.
         int counter = 0;
         while (counter < index) {
@@ -176,9 +178,10 @@ NetworkPacket DoublyLinkedList::remove(int index) {
             counter++;
         }
         // Now we should be at correct spot for removal.
+        // Do bookkeeping.
         curr->prev->next = curr->next;
         curr->next->prev = curr->prev;
-        NetworkPacket pack = curr->getData();
+        T pack = curr->getData();
         delete curr;
         length--;
         return pack;
@@ -186,15 +189,20 @@ NetworkPacket DoublyLinkedList::remove(int index) {
 }
 
 // Implement helper function clear():
-void DoublyLinkedList::clear() {
+template <typename T>
+void DoublyLinkedList<T>::clear() {
     // Iterate through the list and delete each node.
     while (front != nullptr)
     {   // Start at the front and create temp point then delete front.
-        // Shift the front to until reaching null.
-        Node* deleteNode = front;
+        // Shift the front to next until reaching null.
+        Node<T>* deleteNode = front;
         front = front->next;
         delete deleteNode;
     }
     // Take care of dangling pointer.
     back = nullptr;
 }
+
+// Must instantiate the pattern so the compilation will work.
+template class DoublyLinkedList<int>;
+template class DoublyLinkedList<NetworkPacket>;
